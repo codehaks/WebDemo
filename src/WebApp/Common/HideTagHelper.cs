90b2c984414b31a5084bd1bd2c46ee320cbe587d
Hakim Ghods <ghods.hakim@hotmail.com>
2022-01-14 20:40:32 +0330

    feat: add hideTagHelper

---
+++ src/WebApp/Common/HideTagHelper.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace WebApp.Common
+{
+    [HtmlTargetElement(Attributes ="hide")]
+    public class HideTagHelper:TagHelper
+    {
+        [HtmlAttributeName("hide")]
+        public bool IsHidden { get; set; }=false;
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (IsHidden)
+            {
+                output.TagName = null;
+                output.SuppressOutput();
+            }
+        }
+
+        public override int Order => int.MinValue;
+    }
+}
